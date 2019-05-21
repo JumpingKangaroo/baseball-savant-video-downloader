3786d5213ffc28a58653b1b84b50a2ff9d55a6dd
Chris Lambertson <lambertsonchris@gmail.com>
2019-05-20 23:00:50 -0400

    Initial commit

---
+++ fetch.rb
@@ -0,0 +1,6 @@
+
+# Date format: 2019-05-05
+startingDate = '2019-05-05'
+endingDate = '2019-05-11'
+searchURL = "https://baseballsavant.mlb.com/statcast_search?hfPT=&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R|&hfC=&hfSea=2019|&hfSit=&player_type=batter&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=#{startingDate}&game_date_lt=#{endingDate}&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&batters_lookup[]=545361&hfFlag=is\.\.last\.\.pitch|&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_pas=0#results"
+
