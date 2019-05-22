85c2c3930393a7ac02544d475b4f6b1173fd944b
Chris Lambertson <lambertsonchris@gmail.com>
2019-05-22 15:16:47 -0400

    Add user prompt to download webpage, and confirm

--- fetch.rb
+++ fetch.rb
@@ -29,17 +29,16 @@ def download_all_matches(matches)
   end
 end
 
-# Date format: 2019-05-05
-startingDate = '2019-05-05'
-endingDate = '2019-05-11'
-searchURL = "https://baseballsavant.mlb.com/statcast_search?hfPT=&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R|&hfC=&hfSea=2019|&hfSit=&player_type=batter&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=#{startingDate}&game_date_lt=#{endingDate}&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&batters_lookup[]=545361&hfFlag=is\.\.last\.\.pitch|&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_pas=0#results"
+# Prompt user to download webpage
+puts "Go to baseball savant search, and do the search you want. After clicking search, make sure to expand the result by clicking on the player. Right click -> save as a complete page with the default name. Press enter when done"
+gets
 
 # Read in the manually saved webpage
 webpage = File.read('Statcast Search _ baseballsavant.com')
 
 # Find matches and download them
 matches = find_video_links(webpage)
-# download_all_matches(matches)
+download_all_matches(matches)
 
 # Create text file for ffmpeg
 text = ""
