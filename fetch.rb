01e231854977e9ece56f65df15b7952f0184e243
Chris Lambertson <lambertsonchris@gmail.com>
2019-05-23 12:10:07 -0400

    Add get script to fetch webpage with wget. Fetching of urls working, but fetching of vidoes not

--- fetch.rb
+++ fetch.rb
@@ -3,41 +3,40 @@ require 'io/console'
 
 # Parse the given html to find the video links
 def find_video_links(webpageHTML)
-  expression = /(https:\/\/baseballsavant.mlb.com\/sporty-videos.*?)" target="/
+  expression = /"(\/sporty-v.*?)" target/
   webpageHTML.scan expression
 end
 
 # Use youtube-dl to download a given url to a given filename in the vids subfolder
 def download_video(url, name)
-  command = "youtube-dl #{url[0]} -o vids/#{name}.mp4"
+  command = "youtube-dl https://baseballsavant.mlb.com#{url[0]} -o vids/#{name}.mp4"
   result = `#{command}`
 end
 
-# Read in a given filename and return the whole thing
-def read_file(filename)
-  file_data = File.open(filename).read
-  file.close
-  return file_data
-end
-
+# Downloads the list of given URLs with youtbe-dl
 def download_all_matches(matches)
   i = 1
   matches.each do |match|
-    puts "Downloading video #{i} of #{matches.length}"
+    puts "Downloading video #{i} of #{matches.length} with url: https://baseballsavant.mlb.com#{match[0]}"
     download_video(match, i)
     i += 1
   end
 end
 
-# Prompt user to download webpage
-puts "Go to baseball savant search, and do the search you want. After clicking search, make sure to expand the result by clicking on the player. Right click -> save as a complete page with the default name. Press enter when done"
-gets
+# Expects date in the format 2019-05-11
+def get_search_results(startDate, endDate)
+  results = `bash get.sh #{startDate} #{endDate}`
+  File.open("output").read
+end
 
-# Read in the manually saved webpage
-webpage = File.read('Statcast Search _ baseballsavant.com')
+# Get the search results for the given dates
+webpage = get_search_results("2019-05-05", "2019-05-11")
 
 # Find matches and download them
 matches = find_video_links(webpage)
+if matches.length <= 1
+  puts "ERROR, 0 matches found in request"
+end
 download_all_matches(matches)
 
 # Create text file for ffmpeg
